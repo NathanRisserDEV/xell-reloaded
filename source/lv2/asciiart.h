a3af5c4e45ff0afb491d08044a799a53eb3c6789
Nathan Risser <pingmanbot33@gmail.com>
2025-03-06 16:19:17 -0500

    asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,17 +1,13 @@
 static char* asciiart = "\n"                                                                                      
-  " ____            ___                                ________      ____   ____    ____\n"
-"  6MMMMb/          `MM                                `MMMMMMMb.   6MMMMb/ `MM'    `MM'\n"
-" 8P    YM           MM                                 MM    `Mb  8P    YM  MM      MM \n"
-"6M      Y    ___    MM    ___   ____   ___ ____    ___ MM     MM 6M      Y  MM      MM \n"
-"MM         6MMMMb   MM  6MMMMb  `MM(   )P' `MM(    )M' MM     MM MM         MM      MM \n"
-"MM        8M'  `Mb  MM 8M'  `Mb  `MM` ,P    `Mb    d'  MM    .M9 MM         MMMMMMMMMM \n"
-"MM     ___    ,oMM  MM     ,oMM   `MM,P      YM.  ,P   MMMMMMM9' MM     ___ MM      MM \n"
-"MM     `M',6MM9'MM  MM ,6MM9'MM    `MM.       MM  M    MM  \\M\\ MM     `M' MM      MM \n"
-"YM      M MM'   MM  MM MM'   MM    d`MM.      `Mbd'    MM   \\M\\YM      M  MM      MM \n"
-" 8b    d9 MM.  ,MM  MM MM.  ,MM   d' `MM.      YMP     MM    \\M\\8b    d9  MM      MM \n"
-"  YMMMM9  `YMMM9'Yb_MM_`YMMM9'Yb_d_  _)MM_      M     _MM_    \\M\\YMMMM9  _MM_    _MM_\n"
-"                                               d'                                      \n"
-"                                           (8),P                                       \n"
-"                                            YMM                                        \n\n";                                        
+"   ▄████  ▄▄▄       ██▓    ▄▄▄      ▒██   ██▒▓██   ██▓ ██▀███    ▄████  ██░ ██\n"
+" ██▒ ▀█▒▒████▄    ▓██▒   ▒████▄    ▒▒ █ █ ▒░ ▒██  ██▒▓██ ▒ ██▒ ██▒ ▀█▒▓██░ ██▒\n"
+"▒██░▄▄▄░▒██  ▀█▄  ▒██░   ▒██  ▀█▄  ░░  █   ░  ▒██ ██░▓██ ░▄█ ▒▒██░▄▄▄░▒██▀▀██░\n"
+"░▓█  ██▓░██▄▄▄▄██ ▒██░   ░██▄▄▄▄██  ░ █ █ ▒   ░ ▐██▓░▒██▀▀█▄  ░▓█  ██▓░▓█ ░██ \n"
+"░▒▓███▀▒ ▓█   ▓██▒░██████▒▓█   ▓██▒▒██▒ ▒██▒  ░ ██▒▓░░██▓ ▒██▒░▒▓███▀▒░▓█▒░██▓\n"
+" ░▒   ▒  ▒▒   ▓▒█░░ ▒░▓  ░▒▒   ▓▒█░▒▒ ░ ░▓ ░   ██▒▒▒ ░ ▒▓ ░▒▓░ ░▒   ▒  ▒ ░░▒░▒\n"
+"  ░   ░   ▒   ▒▒ ░░ ░ ▒  ░ ▒   ▒▒ ░░░   ░▒ ░ ▓██ ░▒░   ░▒ ░ ▒░  ░   ░  ▒ ░▒░ ░\n"
+"░ ░   ░   ░   ▒     ░ ░    ░   ▒    ░    ░   ▒ ▒ ░░    ░░   ░ ░ ░   ░  ░  ░░ ░\n"
+"      ░       ░  ░    ░  ░     ░  ░ ░    ░   ░ ░        ░           ░  ░  ░  ░\n"
+"                                             ░ ░                              \n\n";                                   
 static char* asciitail =
 	"              GalaxyRgh XeLL RELOADED- Xenon Linux Loader\n\n";
