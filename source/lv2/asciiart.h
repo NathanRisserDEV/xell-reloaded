30fe7242d03c59ccb620ea26ed9d666e2a5a48a2
Nathan Risser <pingmanbot33@gmail.com>
2025-03-08 22:42:54 -0500

    asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,10 +1,27 @@
 static char* asciiart = "\n"                                                                                                                                                                                  
-"##        #######  ##    ##  ######   ##     ##  #######  ########  ##    ##\n"
-"##       ##     ## ###   ## ##    ##  ##     ## ##     ## ##     ## ###   ##\n"
-"##       ##     ## ####  ## ##        ##     ## ##     ## ##     ## ####  ##\n"
-"##       ##     ## ## ## ## ##   #### ######### ##     ## ########  ## ## ##\n"
-"##       ##     ## ##  #### ##    ##  ##     ## ##     ## ##   ##   ##  ####\n"
-"##       ##     ## ##   ### ##    ##  ##     ## ##     ## ##    ##  ##   ###\n"
-"########  #######  ##    ##  ######   ##     ##  #######  ##     ## ##    ##\n\n";
+
+"        GGGGGGGGGGGGG                  lllllll                                                              RRRRRRRRRRRRRRRRR           GGGGGGGGGGGGGHHHHHHHHH     HHHHHHHHH\n"
+"     GGG::::::::::::G                  l:::::l                                                              R::::::::::::::::R       GGG::::::::::::GH:::::::H     H:::::::H\n"
+"   GG:::::::::::::::G                  l:::::l                                                              R::::::RRRRRR:::::R    GG:::::::::::::::GH:::::::H     H:::::::H\n"
+"  G:::::GGGGGGGG::::G                  l:::::l                                                              RR:::::R     R:::::R  G:::::GGGGGGGG::::GHH::::::H     H::::::HH\n"
+" G:::::G       GGGGGG  aaaaaaaaaaaaa    l::::l   aaaaaaaaaaaaa   xxxxxxx      xxxxxxxyyyyyyy           yyyyyyyR::::R     R:::::R G:::::G       GGGGGG  H:::::H     H:::::H\n"
+"G:::::G                a::::::::::::a   l::::l   a::::::::::::a   x:::::x    x:::::x  y:::::y         y:::::y R::::R     R:::::RG:::::G                H:::::H     H:::::H\n"
+"G:::::G                aaaaaaaaa:::::a  l::::l   aaaaaaaaa:::::a   x:::::x  x:::::x    y:::::y       y:::::y  R::::RRRRRR:::::R G:::::G                H::::::HHHHH::::::H\n"
+"G:::::G    GGGGGGGGGG           a::::a  l::::l            a::::a    x:::::xx:::::x      y:::::y     y:::::y   R:::::::::::::RR  G:::::G    GGGGGGGGGG  H:::::::::::::::::H\n"
+"G:::::G    G::::::::G    aaaaaaa:::::a  l::::l     aaaaaaa:::::a     x::::::::::x        y:::::y   y:::::y    R::::RRRRRR:::::R G:::::G    G::::::::G  H:::::::::::::::::H\n"
+"G:::::G    GGGGG::::G  aa::::::::::::a  l::::l   aa::::::::::::a      x::::::::x          y:::::y y:::::y     R::::R     R:::::RG:::::G    GGGGG::::G  H::::::HHHHH::::::H\n"
+"G:::::G        G::::G a::::aaaa::::::a  l::::l  a::::aaaa::::::a      x::::::::x           y:::::y:::::y      R::::R     R:::::RG:::::G        G::::G  H:::::H     H:::::H\n"
+"G:::::G       G::::Ga::::a    a:::::a  l::::l a::::a    a:::::a     x::::::::::x           y:::::::::y       R::::R     R:::::R G:::::G       G::::G  H:::::H     H:::::H\n"
+" G:::::GGGGGGGG::::Ga::::a    a:::::a l::::::la::::a    a:::::a    x:::::xx:::::x           y:::::::y      RR:::::R     R:::::R  G:::::GGGGGGGG::::GHH::::::H     H::::::HH\n"
+"  GG:::::::::::::::Ga:::::aaaa::::::a l::::::la:::::aaaa::::::a   x:::::x  x:::::x           y:::::y       R::::::R     R:::::R   GG:::::::::::::::GH:::::::H     H:::::::H\n"
+"    GGG::::::GGG:::G a::::::::::aa:::al::::::l a::::::::::aa:::a x:::::x    x:::::x         y:::::y        R::::::R     R:::::R     GGG::::::GGG:::GH:::::::H     H:::::::H\n"
+"       GGGGGG   GGGG  aaaaaaaaaa  aaaallllllll  aaaaaaaaaa  aaaaxxxxxxx      xxxxxxx       y:::::y         RRRRRRRR     RRRRRRR        GGGGGG   GGGGHHHHHHHHH     HHHHHHHHH\n"
+"                                                                                          y:::::y\n"
+"                                                                                         y:::::y\n"
+"                                                                                        y:::::y\n"
+"                                                                                       y:::::y\n"
+"                                                                                      yyyyyyy\n\n";
+                                                                                                                                                                            
+                                                                                                                                                                            
 static char* asciitail =
-	"              Longhorn XeLL RELOADED- Xenon Linux Loader by squidwidthe1st on discord https://discord.gg/fu7s3pbKwf\n\n";
+	"              GalaxyRGH XeLL RELOADED System- Xenon Linux Loader by squidwidthe1st and SubZero on discord https://discord.gg/fu7s3pbKwf\n\n";
