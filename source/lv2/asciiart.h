900ecda769e8895e3926398667944587522c93a8
Nathan Risser <pingmanbot33@gmail.com>
2025-07-20 21:10:04 -0400

    asciiart.h

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,19 +1,13 @@
 static char* asciiart = "\n"                                                                                                                                                                                  
 
-"  sSSSSs   .S_SSSs    S.       .S_SSSs     .S S.    .S S.    .S_sSSs      sSSSSs   .S    S.\n"
-" d%%%%SP  .SS~SSSSS   SS.     .SS~SSSSS   .SS SS.  .SS SS.  .SS~YS%%b    d%%%%SP  .SS    SS.\n"
-"d%S'      S%S   SSSS  S%S     S%S   SSSS  S%S S%S  S%S S%S  S%S   `S%b  d%S'      S%S    S%S\n"
-"S%S       S%S    S%S  S%S     S%S    S%S  S%S S%S  S%S S%S  S%S    S%S  S%S       S%S    S%S\n"
-"S&S       S%S SSSS%S  S&S     S%S SSSS%S  S%S S%S  S%S S%S  S%S    d*S  S&S       S%S SSSS%S\n"
-"S&S       S&S  SSS%S  S&S     S&S  SSS%S   SS SS    SS SS   S&S   .S*S  S&S       S&S  SSS&S\n"
-"S&S       S&S    S&S  S&S     S&S    S&S    S_S      S S    S&S_sdSSS   S&S       S&S    S&S\n"
-"S&S sSSs  S&S    S&S  S&S     S&S    S&S   SS~SS     SSS    S&S~YSY%b   S&S sSSs  S&S    S&S\n"
-"S*b `S%%  S*S    S&S  S*b     S*S    S&S  S*S S*S    S*S    S*S   `S%b  S*b `S%%  S*S    S*S\n"
-"S*S   S%  S*S    S*S  S*S.    S*S    S*S  S*S S*S    S*S    S*S    S%S  S*S   S%  S*S    S*S\n"
-" SS_sSSS  S*S    S*S   SSSbs  S*S    S*S  S*S S*S    S*S    S*S    S&S   SS_sSSS  S*S    S*S\n"
-"  Y~YSSY  SSS    S*S    YSSP  SSS    S*S  S*S S*S    S*S    S*S    SSS    Y~YSSY  SSS    S*S\n"
-"                 SP                  SP   SP         SP     SP                           SP\n"
-"                 Y                   Y    Y          Y      Y                            Y\n\n";
+
+"      ::::::::      :::     :::            :::     :::    ::: :::   ::: :::::::::   ::::::::  :::    :::\n" 
+"    :+:    :+:   :+: :+:   :+:          :+: :+:   :+:    :+: :+:   :+: :+:    :+: :+:    :+: :+:    :+:\n"  
+"   +:+         +:+   +:+  +:+         +:+   +:+   +:+  +:+   +:+ +:+  +:+    +:+ +:+        +:+    +:+\n"   
+"  :#:        +#++:++#++: +#+        +#++:++#++:   +#++:+     +#++:   +#++:++#:  :#:        +#++:++#++\n"    
+" +#+   +#+# +#+     +#+ +#+        +#+     +#+  +#+  +#+     +#+    +#+    +#+ +#+   +#+# +#+    +#+\n"     
+"#+#    #+# #+#     #+# #+#        #+#     #+# #+#    #+#    #+#    #+#    #+# #+#    #+# #+#    #+#\n"      
+"########  ###     ### ########## ###     ### ###    ###    ###    ###    ###  ########  ###    ###\n\n";       
                                                                                               
 static char* asciitail =
-	"              GalaxyRGH XeLL RELOADED System- Xenon Linux Loader by squidwidthe1st and SubZero on discord https://discord.gg/fu7s3pbKwf\n\n";
+	"              GalaxyRGH XeLL RELOADED 2.0 System- Xenon Linux Loader by squidwidthe1st and SubZero or kinky_zero on discord https://discord.gg/T7mbdp3rS8\n\n";
