af7ac255f3792db07f7a9d32fa211801d189fc3c
Nathan Risser <pingmanbot33@gmail.com>
2025-09-20 17:07:49 -0400

    main.c

--- source/lv2/main.c
+++ source/lv2/main.c
@@ -38,7 +38,7 @@
 void do_asciiart() {
     char *p = asciiart;
     while (*p)
-	    console_putch(*p++);
+        console_putch(*p++);
     printf(asciitail);
 }
 
@@ -46,11 +46,11 @@ void dumpana() {
     int i;
     for (i = 0; i < 0x100; ++i)
     {
-	    uint32_t v;
-	    xenon_smc_ana_read(i, &v);
-	    printf("0x%08x, ", (unsigned int)v);
-	    if ((i&0x7)==0x7)
-		    printf(" // %02x\n", (unsigned int)(i &~0x7));
+        uint32_t v;
+        xenon_smc_ana_read(i, &v);
+        printf("0x%08x, ", (unsigned int)v);
+        if ((i&0x7)==0x7)
+            printf(" // %02x\n", (unsigned int)(i &~0x7));
     }
 }
 
@@ -61,6 +61,51 @@ int cbldvcount;
 int fgldvcount;
 
 unsigned char stacks[6][0x10000];
+
+/* animation stack for thread */
+static unsigned char anim_stack[0x10000];
+
+/* crude millisecond-ish delay */
+static void delay_ms(unsigned int ms)
+{
+    volatile unsigned int i, j;
+    for (j = 0; j < ms; ++j) {
+        for (i = 0; i < 20000; ++i) {
+            __asm__ volatile ("" ::: "memory");
+        }
+    }
+}
+
+/* animation thread */
+void animate_madeby_task()
+{
+    const char *s = "made by squidwidthe1st on discord";
+    int len = strlen(s);
+    int i;
+
+    printf("\n"); // reserve a line for animation
+
+    for (;;) {
+        // type out
+        for (i = 1; i <= len; ++i) {
+            printf("\r%.*s", i, s);
+            fflush(stdout);
+            delay_ms(60);
+        }
+
+        delay_ms(600);
+
+        // delete
+        for (i = len; i >= 0; --i) {
+            printf("\r%.*s", i, s);
+            printf("%*s", len - i, " ");
+            fflush(stdout);
+            delay_ms(30);
+        }
+
+        delay_ms(300);
+    }
+}
 
 void reset_timebase_task()
 {
@@ -77,12 +122,12 @@ void synchronize_timebases()
     
     int i;
     for(i=1;i<6;++i){
-	    xenon_run_thread_task(i,&stacks[i][0xff00],(void *)reset_timebase_task);
-	    while(xenon_is_thread_task_running(i));
+        xenon_run_thread_task(i,&stacks[i][0xff00],(void *)reset_timebase_task);
+        while(xenon_is_thread_task_running(i));
     }
     
     reset_timebase_task(); // don't forget thread 0
-		    
+            
     std((void*)0x200611a0,0x1ff); // restart timebase
 }
     
@@ -93,17 +138,14 @@ int main(){
     printf("ANA Dump before Init:\n");
     dumpana();
 
-    // linux needs this
     synchronize_timebases();
     
-    // irqs preinit (SMC related)
     *(volatile uint32_t*)0xea00106c = 0x1000000;
     *(volatile uint32_t*)0xea001064 = 0x10;
     *(volatile uint32_t*)0xea00105c = 0xc000000;
 
     xenon_smc_start_bootanim();
 
-    // flush console after each outputted char
     setbuf(stdout,NULL);
 
     xenos_init(VIDEO_MODE_AUTO);
@@ -112,44 +154,39 @@ int main(){
     dumpana();
 
 #ifdef SWIZZY_THEME
-    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_ORANGE); // Orange text on black bg
+    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_ORANGE);
 #elif defined XTUDO_THEME
-    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_PINK); // Pink text on black bg
+    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_PINK);
 #elif defined DEFAULT_THEME
-    console_set_colors(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_PURPLE); // Red text on black bg
+    console_set_colors(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_PURPLE);
 #else
-    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_GREEN); // Green text on black bg
+    console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_GREEN);
 #endif
     console_init();
 
     printf("\nXeLL RELOADED GALAXYRGH - Xenon Linux Loader 2nd Stage " LONGVERSION "\n");
-    //printf("\nBuilt with GCC " GCC_VERSION " and Binutils " BINUTILS_VERSION " \n"); // User doesn't need to know this
     do_asciiart();
-
-    //delay(3); //give the user a chance to see our splash screen <- network init should last long enough...
     
     xenon_sound_init();
     xenon_make_it_faster(XENON_SPEED_FULL);
 
-    if (xenon_get_console_type() != REV_CORONA_PHISON) //Not needed for MMC type of consoles! ;)
+    if (xenon_get_console_type() != REV_CORONA_PHISON)
     {
-	    printf(" * nand init\n");
-	    sfcx_init();
-	    if (sfc.initialized != SFCX_INITIALIZED)
-	    {
-		    printf(" ! sfcx initialization failure\n");
-		    printf(" ! nand related features will not be available\n");
-		    delay(5);
-	    }
+        printf(" * nand init\n");
+        sfcx_init();
+        if (sfc.initialized != SFCX_INITIALIZED)
+        {
+            printf(" ! sfcx initialization failure\n");
+            printf(" ! nand related features will not be available\n");
+            delay(5);
+        }
     }
 
     xenon_config_init();
 
 #ifndef NO_NETWORKING
-
     printf(" * network init\n");
     network_init();
-
     printf(" * starting httpd server...");
     httpd_start();
     printf("success\n");
@@ -171,7 +208,7 @@ int main(){
     /*int device_list_size = */ findDevices();
 
     console_clrscr();
-    printf(" _________________________________________________\n|                                                 |\n|  XeLL RELOADED - Xenon Linux Loader             |\n|  GalaxyRGH build by squidwidthe1st on Discord   |\n|_________________________________________________|\n"); // Fancy
+    printf(" _________________________________________________\n|                                                 |\n|  XeLL RELOADED - Xenon Linux Loader             |\n|  GalaxyRGH build by squidwidthe1st on Discord   |\n|_________________________________________________|\n"); 
     
 #ifndef NO_PRINT_CONFIG
     printf("\n * FUSES to this system - write them down and keep them safe:\n");
@@ -180,22 +217,21 @@ int main(){
     char *fgldvstr = FGLDV;
     
     for (i = 0; i < 12; ++i){
-	    u64 line;
-	    unsigned int hi,lo;
-	    
-	    line = xenon_secotp_read_line(i);
-	    hi=line >> 32;
-	    lo=line & 0xffffffff;
+        u64 line;
+        unsigned int hi,lo;
+        
+        line = xenon_secotp_read_line(i);
+        hi=line >> 32;
+        lo=line & 0xffffffff;
 
-	    fusestr += sprintf(fusestr, "fuseset %02d: %08x%08x\n", i, hi, lo);
+        fusestr += sprintf(fusestr, "fuseset %02d: %08x%08x\n", i, hi, lo);
 
-	    if (i >= 7) {
-		    fgldvstr += sprintf(fgldvstr, "%08x%08x", hi, lo) + '\0';
-	    }
+        if (i >= 7) {
+            fgldvstr += sprintf(fgldvstr, "%08x%08x", hi, lo) + '\0';
+        }
         if (i == 2) {
             cbldvstr += sprintf(cbldvstr, "%08x%08x", hi, lo);
         }
-
     }
 
     for (i = 0; CBLDV[i] != '\0' ; ++i) {
@@ -204,11 +240,10 @@ int main(){
         }
     }
 
-    
     for (i = 0; FGLDV[i] != '\0'; ++i) {
-	    if ('f' == FGLDV[i]) {
-		    ++fgldvcount;
-	    }
+        if ('f' == FGLDV[i]) {
+            ++fgldvcount;
+        }
     }
     
     printf(FUSES);
@@ -219,27 +254,30 @@ int main(){
     printf(" * CPU PVR: %08x\n", mfspr(287));
     
     if (xenon_get_console_type() == 0) {
-	    printf(" * Console: Xenon System\n");
+        printf(" * Console: Xenon System\n");
     } else if (xenon_get_console_type() == 1) {
-	    printf(" * Console: Xenon/Zephyr System\n");
+        printf(" * Console: Xenon/Zephyr System\n");
     } else if (xenon_get_console_type() == 2) {
-	    printf(" * Console: Falcon System\n");
-		printf(" * Glitch Method: RGH-1.2\n");
-	    printf(" * Glitch Chip: Matrix Glitcher\n");
-	    printf(" * Modder: squidwidthe1st on discord\n");
-	    printf(" * Mod Date: 9/2/2025\n");
+        printf(" * Console: Falcon System\n");
     } else if (xenon_get_console_type() == 3) {
-	    printf(" * Console: Jasper System\n");
+        printf(" * Console: Jasper System\n");
     } else if (xenon_get_console_type() == 4) {
-	    printf(" * Console: Trinity System\n");
+        printf(" * Console: Trinity System\n");
+        printf(" * Glitch Method: RGH-3\n");
+        printf(" * Glitch Chip: NONE-Chipless | 10k Resistor\n");
+
+        // start animated thread instead of static modder line
+        xenon_run_thread_task(5, &anim_stack[0xff00], (void *)animate_madeby_task);
+
+        printf(" * Mod Date: 9/20/2025\n");
     } else if (xenon_get_console_type() == 5) {
-	    printf(" * Console: Corona System\n");
+        printf(" * Console: Corona System\n");
     } else if (xenon_get_console_type() == 6) {
-	    printf(" * Console: Corona MMC System\n");
+        printf(" * Console: Corona MMC System\n");
     } else if (xenon_get_console_type() == 7) {
-	    printf(" * Console: Winchester - how did you get here??? really how?\n");
+        printf(" * Console: Winchester - how did you get here??? really how?\n");
     } else if (xenon_get_console_type() == -1) {
-	    printf(" * Console: Unknown ERROR\n");
+        printf(" * Console: Unknown ERROR\n");
     }
     
     printf(" * 2BL LDV: %d\n", cbldvcount);
@@ -247,35 +285,16 @@ int main(){
     
     network_print_config();
 #endif
-    /* Stop logging and save it to first USB Device found that is writeable */
     LogDeInit();
-    //extern char device_list[STD_MAX][10];
-
-    //for (i = 0; i < device_list_size; i++)
-    //{
-    //	if (strncmp(device_list[i], "ud", 2) == 0)
-    //	{
-    //		char tmp[STD_MAX + 8];
-    //		sprintf(tmp, "%sxell.log", device_list[i]);
-    //		if (LogWriteFile(tmp) == 0)
-    //			i = device_list_size;
-    //	}
-    //}
     
     mount_all_devices();
     printf("\n * Looking for files on local media and TFTP...\n\n");
     for(;;){
-	    fileloop();
-	    tftp_loop(); //less likely to find something...
-	    console_clrline();
-	    usb_do_poll(); // Refresh USB
+        fileloop();
+        tftp_loop();
+        console_clrline();
+        usb_do_poll();
     }
 
     return 0;
 }
-
-
-
-
-
-
