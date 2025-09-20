5a3b077b435d6f15250a8099b76ba3962245cd43
Nathan Risser <pingmanbot33@gmail.com>
2025-09-20 17:19:55 -0400

    main.c

--- source/lv2/main.c
+++ source/lv2/main.c
@@ -35,48 +35,10 @@
 
 #include "log.h"
 
-// ---------------------- animation code ----------------------
-void animate_signature_step() {
-    static const char *msg = "Made by squidwidthe1st on Discord";
-    static int len = 0;
-    static int pos = 0;
-    static int direction = 1; // 1 = typing forward, -1 = deleting
-    static int tick = 0;
-
-    if (len == 0) len = strlen(msg);
-
-    // control animation speed (adjust ticks if too fast/slow)
-    if (++tick < 3) return;
-    tick = 0;
-
-    // fixed row for animation
-    int row = 25;
-    int col = 0;
-
-    // clear line before writing
-    console_set_cursor(row, col);
-    printf("%-*s", len, "");
-
-    // print substring
-    console_set_cursor(row, col);
-    printf("%.*s", pos, msg);
-
-    // update typing position
-    pos += direction;
-    if (pos > len) {
-        direction = -1;
-        pos = len;
-    } else if (pos < 0) {
-        direction = 1;
-        pos = 0;
-    }
-}
-// ------------------------------------------------------------
-
 void do_asciiart() {
     char *p = asciiart;
     while (*p)
-        console_putch(*p++);
+	    console_putch(*p++);
     printf(asciitail);
 }
 
@@ -84,11 +46,11 @@ void dumpana() {
     int i;
     for (i = 0; i < 0x100; ++i)
     {
-        uint32_t v;
-        xenon_smc_ana_read(i, &v);
-        printf("0x%08x, ", (unsigned int)v);
-        if ((i&0x7)==0x7)
-            printf(" // %02x\n", (unsigned int)(i &~0x7));
+	    uint32_t v;
+	    xenon_smc_ana_read(i, &v);
+	    printf("0x%08x, ", (unsigned int)v);
+	    if ((i&0x7)==0x7)
+		    printf(" // %02x\n", (unsigned int)(i &~0x7));
     }
 }
 
@@ -115,12 +77,12 @@ void synchronize_timebases()
     
     int i;
     for(i=1;i<6;++i){
-        xenon_run_thread_task(i,&stacks[i][0xff00],(void *)reset_timebase_task);
-        while(xenon_is_thread_task_running(i));
+	    xenon_run_thread_task(i,&stacks[i][0xff00],(void *)reset_timebase_task);
+	    while(xenon_is_thread_task_running(i));
     }
     
     reset_timebase_task(); // don't forget thread 0
-            
+		    
     std((void*)0x200611a0,0x1ff); // restart timebase
 }
     
@@ -154,33 +116,37 @@ int main(){
 #elif defined XTUDO_THEME
     console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_PINK); // Pink text on black bg
 #elif defined DEFAULT_THEME
-    console_set_colors(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_PURPLE); // Purple text on black bg
+    console_set_colors(CONSOLE_COLOR_BLACK, CONSOLE_COLOR_PURPLE); // Red text on black bg
 #else
     console_set_colors(CONSOLE_COLOR_BLACK,CONSOLE_COLOR_GREEN); // Green text on black bg
 #endif
     console_init();
 
     printf("\nXeLL RELOADED GALAXYRGH - Xenon Linux Loader 2nd Stage " LONGVERSION "\n");
+    //printf("\nBuilt with GCC " GCC_VERSION " and Binutils " BINUTILS_VERSION " \n"); // User doesn't need to know this
     do_asciiart();
 
+    //delay(3); //give the user a chance to see our splash screen <- network init should last long enough...
+    
     xenon_sound_init();
     xenon_make_it_faster(XENON_SPEED_FULL);
 
     if (xenon_get_console_type() != REV_CORONA_PHISON) //Not needed for MMC type of consoles! ;)
     {
-        printf(" * nand init\n");
-        sfcx_init();
-        if (sfc.initialized != SFCX_INITIALIZED)
-        {
-            printf(" ! sfcx initialization failure\n");
-            printf(" ! nand related features will not be available\n");
-            delay(5);
-        }
+	    printf(" * nand init\n");
+	    sfcx_init();
+	    if (sfc.initialized != SFCX_INITIALIZED)
+	    {
+		    printf(" ! sfcx initialization failure\n");
+		    printf(" ! nand related features will not be available\n");
+		    delay(5);
+	    }
     }
 
     xenon_config_init();
 
 #ifndef NO_NETWORKING
+
     printf(" * network init\n");
     network_init();
 
@@ -202,7 +168,7 @@ int main(){
 #endif
 
     mount_all_devices();
-    findDevices();
+    /*int device_list_size = */ findDevices();
 
     console_clrscr();
     printf(" _________________________________________________\n|                                                 |\n|  XeLL RELOADED - Xenon Linux Loader             |\n|  GalaxyRGH build by squidwidthe1st on Discord   |\n|_________________________________________________|\n"); // Fancy
@@ -214,21 +180,22 @@ int main(){
     char *fgldvstr = FGLDV;
     
     for (i = 0; i < 12; ++i){
-        u64 line;
-        unsigned int hi,lo;
-        
-        line = xenon_secotp_read_line(i);
-        hi=line >> 32;
-        lo=line & 0xffffffff;
+	    u64 line;
+	    unsigned int hi,lo;
+	    
+	    line = xenon_secotp_read_line(i);
+	    hi=line >> 32;
+	    lo=line & 0xffffffff;
 
-        fusestr += sprintf(fusestr, "fuseset %02d: %08x%08x\n", i, hi, lo);
+	    fusestr += sprintf(fusestr, "fuseset %02d: %08x%08x\n", i, hi, lo);
 
-        if (i >= 7) {
-            fgldvstr += sprintf(fgldvstr, "%08x%08x", hi, lo) + '\0';
-        }
+	    if (i >= 7) {
+		    fgldvstr += sprintf(fgldvstr, "%08x%08x", hi, lo) + '\0';
+	    }
         if (i == 2) {
             cbldvstr += sprintf(cbldvstr, "%08x%08x", hi, lo);
         }
+
     }
 
     for (i = 0; CBLDV[i] != '\0' ; ++i) {
@@ -237,10 +204,11 @@ int main(){
         }
     }
 
+    
     for (i = 0; FGLDV[i] != '\0'; ++i) {
-        if ('f' == FGLDV[i]) {
-            ++fgldvcount;
-        }
+	    if ('f' == FGLDV[i]) {
+		    ++fgldvcount;
+	    }
     }
     
     printf(FUSES);
@@ -251,27 +219,27 @@ int main(){
     printf(" * CPU PVR: %08x\n", mfspr(287));
     
     if (xenon_get_console_type() == 0) {
-        printf(" * Console: Xenon System\n");
+	    printf(" * Console: Xenon System\n");
     } else if (xenon_get_console_type() == 1) {
-        printf(" * Console: Xenon/Zephyr System\n");
+	    printf(" * Console: Xenon/Zephyr System\n");
     } else if (xenon_get_console_type() == 2) {
-        printf(" * Console: Falcon System\n");
+	    printf(" * Console: Falcon System\n");
     } else if (xenon_get_console_type() == 3) {
-        printf(" * Console: Jasper System\n");
+	    printf(" * Console: Jasper System\n");
     } else if (xenon_get_console_type() == 4) {
-        printf(" * Console: Trinity System\n");
-        printf(" * Glitch Method: RGH-3\n");
-        printf(" * Glitch Chip: NONE-Chipless | 10k Resistor\n");
-        printf(" * Modder: squidwidthe1st on discord\n");
-        printf(" * Mod Date: 9/20/2025\n");
+	    printf(" * Console: Trinity System\n");
+		printf(" * Glitch Method: RGH-3\n");
+	    printf(" * Glitch Chip: NONE-Chipless | 10k Resistor\n");
+	    printf(" * Modder: squidwidthe1st on discord\n");
+	    printf(" * Mod Date: 9/20/2025\n");
     } else if (xenon_get_console_type() == 5) {
-        printf(" * Console: Corona System\n");
+	    printf(" * Console: Corona System\n");
     } else if (xenon_get_console_type() == 6) {
-        printf(" * Console: Corona MMC System\n");
+	    printf(" * Console: Corona MMC System\n");
     } else if (xenon_get_console_type() == 7) {
-        printf(" * Console: Winchester - how did you get here??? really how?\n");
+	    printf(" * Console: Winchester - how did you get here??? really how?\n");
     } else if (xenon_get_console_type() == -1) {
-        printf(" * Console: Unknown ERROR\n");
+	    printf(" * Console: Unknown ERROR\n");
     }
     
     printf(" * 2BL LDV: %d\n", cbldvcount);
@@ -279,21 +247,36 @@ int main(){
     
     network_print_config();
 #endif
+    /* Stop logging and save it to first USB Device found that is writeable */
     LogDeInit();
+    //extern char device_list[STD_MAX][10];
+
+    //for (i = 0; i < device_list_size; i++)
+    //{
+    //	if (strncmp(device_list[i], "ud", 2) == 0)
+    //	{
+    //		char tmp[STD_MAX + 8];
+    //		sprintf(tmp, "%sxell.log", device_list[i]);
+    //		if (LogWriteFile(tmp) == 0)
+    //			i = device_list_size;
+    //	}
+    //}
     
     mount_all_devices();
     printf("\n * Looking for files on local media and TFTP...\n\n");
-
-    // ------------ main loop ------------
     for(;;){
-        fileloop();
-        tftp_loop();
-        usb_do_poll();
-
-        // animate on bottom line
-        animate_signature_step();
+	    fileloop();
+	    tftp_loop(); //less likely to find something...
+	    console_clrline();
+	    usb_do_poll(); // Refresh USB
     }
-    // ----------------------------------
 
     return 0;
 }
+
+
+
+
+
+
+
