50465b38cecfe2c17aa75469ce3e45b9913dd62f
Nathan Risser <pingmanbot33@gmail.com>
2026-07-10 17:49:40 -0400

    splash.c

--- source/lv2/splash.c
+++ source/lv2/splash.c
@@ -2,11 +2,32 @@
 
 #include <console/console.h>
 #include <time/time.h>
+#include <xenos/xenos.h>
 
 #include "splash.h"
 #include "splash_image.h"
 
 #define SPLASH_MAX_PERCENT 80
+
+struct ati_info {
+    uint32_t unknown1[4];
+    uint32_t base;
+    uint32_t unknown2[8];
+    uint32_t width;
+    uint32_t height;
+} __attribute__ ((__packed__));
+
+static void splash_pset(int x, int y, unsigned char r, unsigned char g, unsigned char b) {
+    struct ati_info *ai = (struct ati_info*)0xec806100ULL;
+    uint32_t *fb = (uint32_t*)(long)(ai->base | 0x80000000);
+    int console_width = ((ai->width + 31) >> 5) << 5;
+    uint32_t color = (b << 24) + (g << 16) + (r << 8);
+    int base = (((y >> 5) * 32 * console_width + ((x >> 5) << 10)
+        + (x & 3) + ((y & 1) << 2) + (((x & 31) >> 2) << 3)
+        + (((y & 31) >> 1) << 6)) ^ ((y & 8) << 2);
+
+    fb[base] = color;
+}
 
 static unsigned char rgb565_red(uint16_t pixel) {
     unsigned char value = (pixel >> 8) & 0xf8;
@@ -24,6 +45,9 @@ static unsigned char rgb565_blue(uint16_t pixel) {
 }
 
 static void draw_scaled_splash(int origin_x, int origin_y, int target_width, int target_height) {
+    struct ati_info *ai = (struct ati_info*)0xec806100ULL;
+    int offset_x = xenos_is_overscan() ? (int)(ai->width / 28) : 0;
+    int offset_y = xenos_is_overscan() ? (int)(ai->height / 28) : 0;
     int y;
 
     for (y = 0; y < target_height; ++y) {
@@ -37,9 +61,9 @@ static void draw_scaled_splash(int origin_x, int origin_y, int target_width, int
             if (pixel == 0)
                 continue;
 
-            console_pset(
-                origin_x + x,
-                origin_y + y,
+            splash_pset(
+                origin_x + offset_x + x,
+                origin_y + offset_y + y,
                 rgb565_red(pixel),
                 rgb565_green(pixel),
                 rgb565_blue(pixel)
